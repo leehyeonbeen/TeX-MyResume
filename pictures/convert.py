698ff9db729b5f888c27f801d9226febc0d5ec42
HB's Macbook Pro <david.hyeonbeen.lee@gmail.com>
2024-02-01 15:42:10 +0900

    cdg_hannam

--- pictures/convert.py
+++ pictures/convert.py
@@ -1,10 +1,21 @@
 import cv2
 import glob
+from PIL import Image
+from pillow_heif import register_heif_opener
 
-for p in glob.glob('pictures/styled_pics/*.jpeg'):
-    img=cv2.imread(p)
-    path=p.split('/')
-    path.insert(-1, 'jpg')
-    path='/'.join(path).replace('jpeg','jpg')
-    cv2.imwrite(path,img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    pass
+register_heif_opener()
+
+jpegs = glob.glob("pictures/styled_pics/*.JPEG")
+heics = glob.glob("pictures/styled_pics/*.HEIC")
+
+for p in jpegs + heics:
+    filepath = ".".join(p.split(".")[:-1])
+    img = Image.open(p)
+    img.convert("RGB").save(f"{filepath}.jpg")
+    print(f"Converted: {filepath}.jpg")
+    # img = cv2.imread(p)
+    # path = p.split("/")
+    # path.insert(-1, "jpg")
+    # path = "/".join(path).replace("jpeg", "jpg")
+    # cv2.imwrite(path, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    # pass
