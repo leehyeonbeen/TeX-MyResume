82fe94882dde8ec279631a92f41c074e47ada09a
HB's Macbook Pro <david.hyeonbeen.lee@gmail.com>
2024-02-02 20:42:27 +0900

    update pics

--- pictures/convert.py
+++ pictures/convert.py
@@ -2,6 +2,7 @@ import cv2
 import glob
 from PIL import Image
 from pillow_heif import register_heif_opener
+import os
 
 register_heif_opener()
 
@@ -11,12 +12,16 @@ jpegs = glob.glob("pictures/styled_pics/*.JPEG") + glob.glob(
 heics = glob.glob("pictures/styled_pics/*.HEIC") + glob.glob(
     "pictures/styled_pics/*.heic"
 )
+raw_pics = glob.glob("pictures/styled_pics/raw/*")
 
-for p in jpegs + heics:
-    filepath = ".".join(p.split(".")[:-1])
+for p in raw_pics:
+    filesize = os.path.getsize(p)
+    savepath = ".".join(p.split(".")[:-1]).replace("raw/", "")
     img = Image.open(p)
-    img.convert("RGB").save(f"{filepath}.jpg")
-    print(f"Converted: {filepath}.jpg")
+    if filesize > 700 * 1024:
+        quality = 700 * 1024 / filesize * 100
+        img.convert("RGB").save(f"{savepath}.jpg", quality=int(quality))
+    print(f"Converted: {savepath}.jpg")
     # img = cv2.imread(p)
     # path = p.split("/")
     # path.insert(-1, "jpg")
